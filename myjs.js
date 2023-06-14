c10b833242b8173b0b2ee76f20d8b0814b12e5c5
RRealJ <90974187+RRealJ@users.noreply.github.com>
2023-06-14 00:11:02 -0300

    só o grosso e layout da calculadora
    somente fiz o css e html, começando a colocar o js, estou vendo se compensa colocar JQuery ou não :p

---
+++ myjs.js
@@ -0,0 +1,16 @@
+
+
+	function addNum(paragrafo) {
+	    var numero = paragrafo.innerHTML;
+	    
+	}
+
+
+	function operacao(paragrafo){
+		var numero = paragrafo.innerHTML;
+
+	}
+
+	function resultado(){
+
+	}
